004f5c9882c80d3508f4f2230210ef85070d6865
visual2 <visual_2@hanmail.net>
2018-04-10 18:34:58 +0900

    test demo6

--- src/main/java/spring/security/demo/web/HomeController.java
+++ src/main/java/spring/security/demo/web/HomeController.java
@@ -1,7 +1,27 @@
 package spring.security.demo.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+    @GetMapping("/")
+    public String root() {
+        return "index";
+    }
 
+    @GetMapping("/user")
+    public String userIndex() {
+        return "user/index";
+    }
 
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 
+    @GetMapping("/access-denied")
+    public String accessDenied() {
+        return "/error/access-denied";
+    }
 }
